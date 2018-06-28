1d8dea53df68492b1eeda091e3d4c3a2616e80ac
ingbyr <ingbyr@outlook.com>
2018-06-28 10:59:08 +0800

    init

---
+++ WebDesigner/src/main/resources/static/js/plugin-get-remote-data.js
@@ -0,0 +1,12 @@
+import grapesjs from './grapes.min.js'
+
+export default grapesjs.plugins.add('get-remote-data', (editor, options) => {
+    /*
+    * Here you should rely on GrapesJS APIs, so check 'API Reference' for more info
+    * For example, you could do something like this to add some new command:
+    *
+    * editor.Commands.add(...);
+    */
+
+    console.log('ingbyr')
+})
