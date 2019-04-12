b58c365a007a1ee36f2e45a5784ab93bce0370dc
ingbyr <ingbyr@outlook.com>
2019-04-12 19:04:31 +0800

    backup old project

--- deploy-designer.sh
+++ deploy-designer.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 echo "deploy ezappx-designer ..."
-scp EzappxDesigner/build/libs/EzappxDesigner-1.0.0.jar ing@www.ezappx.com:~/ezappx
+scp EzappxDesigner/build/libs/EzappxDesigner-1.0.0.jar ezappx@10.109.252.210:~/ezappx
